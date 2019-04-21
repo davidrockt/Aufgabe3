a55fb202f6d222ef850ec52541ad949ddf2db046
davidwe <hi-davidw@web.de>
2019-04-21 15:37:02 +0200

    teste andere App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,29 @@
 import io.javalin.Javalin;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
+
+    private static Map<String, String> reservations = new HashMap<String, String>() {{
+        put("saturday", "No reservation");
+        put("sunday", "No reservation");
+    }};
+
     public static void main(String[] args) {
         Javalin app = Javalin.create()
             .enableStaticFiles("/public")
             .start(7070);
-        
-        app.get("/input", ctx -> {
-            // System.out.println("ctx.queryParam(\"currency\") = " + ctx.queryParam("currency"));
-            ctx.html("Does this work?");
+
+        app.post("/make-reservation", ctx -> {
+            reservations.put(ctx.formParam("day"), ctx.formParam("time"));
+            ctx.html("Your reservation has been saved");
+        });
+
+        /* Die für einen Tag hinterlegte Uhrzeit wird abgefragt.
+         */
+        app.get("/check-reservation", ctx -> {
+            ctx.html("hello people");
         });
     }
 }
